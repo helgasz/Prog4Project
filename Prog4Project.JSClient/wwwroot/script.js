45a5e266136f98043a95b70fb070a433c19b9c93
Szebényi Helga <szebenyihelga@gmail.com>
2023-05-31 18:55:47 +0200

    .

--- Prog4Project.JSClient/wwwroot/script.js
+++ Prog4Project.JSClient/wwwroot/script.js
@@ -1,17 +1,41 @@
 ﻿let workers = [];
-
-fetch('http://localhost:20741/worker')
-    .then(x => x.json())
-    .then(y => {
-        workers = y;
-        console.log(workers);
-        display();
-    });
-
+getdata();
 
 function display() {
     workers.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
-            "<tr><td>" + t.workerId + "</td><td>" + t.workerName + "</td></tr>";
+            '<tr><td>' + t.workerId + '</td><td><center>' + t.workerName + '</center></td><td>' +
+            `<button type="button" onclick="remove(${t.workerId})">Delete</button>` + '</td></tr>';
     });
+}
+
+async function getdata() {
+    await fetch('http://localhost:20741/worker')
+        .then(x => x.json())
+        .then(y => {
+            workers = y;
+            console.log(workers);
+            display();
+        });
+}
+
+function remove(id) {
+    alert(id);
+}
+
+function create() {
+    let name = document.getElementById('workername').value;
+    fetch('http://localhost:20741/worker', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { workername: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+    
 }
